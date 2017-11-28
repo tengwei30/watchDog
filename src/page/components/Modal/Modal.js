de7a5ba632bfe7343060997d6b7e54159d630486
zhangsong <zhangsong@guanghe.tv>
2017-11-28 11:44:33 +0800

    1

--- src/page/components/Modal/Modal.js
+++ src/page/components/Modal/Modal.js
@@ -107,8 +107,100 @@ class showModal extends React.Component{
             },
           });
     }
-    render() {
+    _renderForm () {
         const { getFieldDecorator } = this.props.form;
+        const { isModalData, modalData, visible } = this.props.modalStore
+        return (
+            <Form onSubmit={this.handleSubmit}>
+            <FormItem>
+                <Col span={11}>
+                    <FormItem>
+                        {getFieldDecorator('beginTime',{
+                            initialValue: isModalData.beginTime ? moment(isModalData.beginTime).format('YYYY-MM-DD HH:mm:ss') : moment(isModalData.time).format('YYYY-MM-DD HH:mm:ss')
+                        })(
+                            <Select
+                            placeholder="开始时间"
+                            onChange={this.handleSelectChangeBegin}
+                            >
+                           {
+                              (modalData.times) ? (
+                                modalData.times.map(item => {
+                                    return(
+                                       <Option key={moment(item.time).format('YYYY-MM-DD HH:mm:ss')}>{moment(item.time).format('HH:mm')}</Option>
+                                    )
+                                })
+                               ) : (null)
+                           }
+                            </Select>
+                        )}
+                    </FormItem>
+                </Col>
+                <Col span={2}>
+                <span style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}>
+                    -
+                </span>
+                </Col>
+                <Col span={11}>
+                    <FormItem>
+                        {getFieldDecorator('endTime',{
+                            initialValue: isModalData.endTime ? moment(isModalData.endTime).format('YYYY-MM-DD HH:mm:ss') : moment(isModalData.time).format('YYYY-MM-DD HH:mm:ss')
+                        })(
+                            <Select
+                            placeholder="结束时间"
+                            onChange={this.handleSelectChangeEnd}
+                            >
+                                {
+                                    (modalData.times) ? (
+                                        modalData.times.map(item => {
+                                            return(
+                                            <Option key={moment(item.time).format('YYYY-MM-DD HH:mm:ss')}>{moment(item.time).format('HH:mm')}</Option>
+                                            )
+                                        
+                                        })
+                                    ) : (null)
+                                } 
+                            </Select>
+                        )}
+                    </FormItem>
+                </Col>
+                
+            </FormItem>
+            <FormItem>
+                {getFieldDecorator('description',{
+                    initialValue: isModalData.description ? isModalData.description : '' 
+                })(
+                    <TextArea style={{resize:'none'}} rows={2} placeholder="请添写使用者和主题" />
+                )}
+            </FormItem>
+            <FormItem span={12}>
+                {
+                    (isModalData.id) ? (
+                        <div>
+                            <Col span={11}>
+                                <Button style={{width:'100%'}} type="default" onClick={this.DeleteMessage}>
+                                    删除
+                                </Button>
+                            </Col>
+                            <Col span={2}></Col>
+                            <Col span={11}>
+                                <Button style={{width:'100%'}} type="primary" htmlType="submit">
+                                    提交
+                                </Button>
+                            </Col>
+                        </div>
+                    ): (
+                        <Button style={{width:'100%'}} type="primary" htmlType="submit">
+                            提交
+                        </Button> 
+                    )
+                }
+            </FormItem>
+            
+        </Form>
+        )
+    }
+    render() {
+        
         const { isModalData, modalData, visible } = this.props.modalStore
         return (
             <Modal
@@ -119,92 +211,7 @@ class showModal extends React.Component{
                 footer={[]}
                 className="showModal"
             >
-                <Form onSubmit={this.handleSubmit}>
-                    <FormItem>
-                        <Col span={11}>
-                            <FormItem>
-                                {getFieldDecorator('beginTime',{
-                                    initialValue: isModalData.beginTime ? moment(isModalData.beginTime).format('YYYY-MM-DD HH:mm:ss') : moment(isModalData.time).format('YYYY-MM-DD HH:mm:ss')
-                                })(
-                                    <Select
-                                    placeholder="开始时间"
-                                    onChange={this.handleSelectChangeBegin}
-                                    >
-                                   {
-                                      (modalData.times) ? (
-                                        modalData.times.map(item => {
-                                            return(
-                                               <Option key={moment(item.time).format('YYYY-MM-DD HH:mm:ss')}>{moment(item.time).format('HH:mm')}</Option>
-                                            )
-                                        })
-                                       ) : (null)
-                                   }
-                                    </Select>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={2}>
-                        <span style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}>
-                            -
-                        </span>
-                        </Col>
-                        <Col span={11}>
-                            <FormItem>
-                                {getFieldDecorator('endTime',{
-                                    initialValue: isModalData.endTime ? moment(isModalData.endTime).format('YYYY-MM-DD HH:mm:ss') : moment(isModalData.time).format('YYYY-MM-DD HH:mm:ss')
-                                })(
-                                    <Select
-                                    placeholder="结束时间"
-                                    onChange={this.handleSelectChangeEnd}
-                                    >
-                                        {
-                                            (modalData.times) ? (
-                                                modalData.times.map(item => {
-                                                    return(
-                                                    <Option key={moment(item.time).format('YYYY-MM-DD HH:mm:ss')}>{moment(item.time).format('HH:mm')}</Option>
-                                                    )
-                                                
-                                                })
-                                            ) : (null)
-                                        } 
-                                    </Select>
-                                )}
-                            </FormItem>
-                        </Col>
-                        
-                    </FormItem>
-                    <FormItem>
-                        {getFieldDecorator('description',{
-                            initialValue: isModalData.description ? isModalData.description : '' 
-                        })(
-                            <TextArea style={{resize:'none'}} rows={2} placeholder="请添写使用者和主题" />
-                        )}
-                    </FormItem>
-                    <FormItem span={12}>
-                        {
-                            (isModalData.id) ? (
-                                <div>
-                                    <Col span={11}>
-                                        <Button style={{width:'100%'}} type="default" onClick={this.DeleteMessage}>
-                                            删除
-                                        </Button>
-                                    </Col>
-                                    <Col span={2}></Col>
-                                    <Col span={11}>
-                                        <Button style={{width:'100%'}} type="primary" htmlType="submit">
-                                            提交
-                                        </Button>
-                                    </Col>
-                                </div>
-                            ): (
-                                <Button style={{width:'100%'}} type="primary" htmlType="submit">
-                                    提交
-                                </Button> 
-                            )
-                        }
-                    </FormItem>
-                    
-                </Form>
+                {this._renderForm()}
             </Modal>
         )
     }
