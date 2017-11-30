f63f9b15aad571425e8c2cce5f25c09f83297084
zhangsong <zhangsong@guanghe.tv>
2017-11-30 11:59:11 +0800

    代码整理

--- src/page/components/Header/Header.jsx
+++ src/page/components/Header/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {Button} from 'antd';
 import './Header.css';
 import { inject, observer } from 'mobx-react';
 import {autorun} from 'mobx';
 import iconUser from '../../../../assets/images/iconUser.png';
 import iconLocation from '../../../../assets/images/iconLocation.png';
 import iconComputer from '../../../../assets/images/iconComputer.png';
+import moment from 'moment';
 
-@inject('menuStore')
+@inject('menuStore','chartStore')
 @observer
 export default class Header extends React.Component{
     constructor (props) {
         super(props)
+    }
+    switch = () => { // 本周/下周
+        this.props.chartStore.setSwitchTime(!this.props.chartStore.switchTime)
     }
     render() {
         const {RoomDetail} = this.props.menuStore
@@ -32,6 +37,10 @@ export default class Header extends React.Component{
                         <img src={iconComputer} alt=""/>
                         <i>设备：<em>{RoomDetail.devices}</em></i>
                     </span>
+                </div><br />
+                <div className="btnDate">
+                    <p>今天：{moment().locale('zh-cn').format('dddd')}</p>
+                    <p><Button type="primary" onClick={this.switch}>{this.props.chartStore.switchTime ? '下周' : '本周' }</Button></p>
                 </div>
             </div>
         )
